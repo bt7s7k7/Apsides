254904c2fbabd374a1b9c654bdd8023353e891fc
Branislav Trstenský <bt7s7k7@hotmail.sk>
2024-10-26 14:51:08 +0200

    Hoisted builder script from example

--- ucpem.js
+++ ucpem.js
@@ -4,7 +4,8 @@
 const { project, github, join, constants, run, copy, getProjectDetails, log, include } = require("ucpem")
 const { readdir, writeFile, mkdir, rm, readFile } = require("fs/promises")
 const { extname, basename } = require("path")
-const { readFileSync } = require("fs")
+const { readFileSync, statSync } = require("fs")
+const { spawn } = require("child_process")
 
 include("examples/todo/ucpem.js")
 
@@ -262,3 +263,171 @@ project.script("build-clean", async () => {
         }
     }
 })
+
+project.script("builder", async (args) => {
+    const mode = args[0] == "build" ? "build" : args[0] == "dev" ? "dev" : args[0] == "watch" ? "watch" : null
+    if (mode == null) throw new Error("Invalid mode, expected build, watch or dev")
+
+    const root = constants.installPath
+
+    async function buildBackend(/** @type {boolean} */ isDev, /** @type {import("esbuild").Plugin | null} */ plugin = null) {
+        const { build, context } = require("esbuild")
+
+        await rm(join(root, "build"), { force: true, recursive: true })
+
+        /** @type {Parameters<typeof build>[0]} */
+        const options = {
+            bundle: true,
+            format: "esm",
+            entryPoints: ["./src/index.ts"],
+            outfile: "build/index.mjs",
+            sourcemap: true,
+            logLevel: isDev ? "silent" : "info",
+            platform: "node",
+            preserveSymlinks: true,
+            packages: isDev ? "external" : "bundle",
+            define: {
+                "import.meta.env.MODE": isDev ? JSON.stringify("development") : JSON.stringify("production"),
+                "import.meta.env.DEV": JSON.stringify(isDev),
+                "import.meta.env.PROD": JSON.stringify(!isDev),
+            },
+            supported: {
+                "using": false
+            },
+        }
+
+        if (plugin != null) {
+            options.plugins = [plugin]
+        }
+
+        if (isDev) {
+            const watcher = await context(options)
+            return watcher
+        }
+
+        await build(options)
+    }
+
+    if (mode == "build") {
+        const packageJSON = JSON.parse((await readFile(join(root, "package.json"))).toString())
+
+        await buildBackend(false)
+
+        try {
+            await copy(join(root, "dist"), join(root, "build/frontend/dist"), { quiet: true })
+        } catch {
+            log("Missing frontend folder")
+        }
+
+        await writeFile(join(root, "build/package.json"), JSON.stringify({
+            ...packageJSON,
+            devDependencies: undefined,
+            dependencies: undefined,
+            scripts: {
+                "start": "node --enable-source-maps index.mjs"
+            },
+            main: "index.mjs"
+        }, null, 4))
+
+        return
+    }
+
+    async function watchBackend() {
+        let lastDate = NaN
+        let date = NaN
+
+        const esbuildContext = await buildBackend(true, {
+            name: "watch-runner",
+            setup(build) {
+                build.onEnd(() => {
+                    lastDate = date
+                    date = statSync("./build/index.mjs").ctimeMs
+                })
+            }
+        })
+
+        /** @type {import("child_process").ChildProcess | null} */
+        let child = null
+
+        function log(/** @type {string} */ msg) {
+            // eslint-disable-next-line no-console
+            console.log("\x1b[96m[backend] " + msg + "\x1b[0m")
+        }
+
+        /** @type {ReturnType<typeof setTimeout> | null} */
+        let debounceTimerId = null
+
+        function execute() {
+            if (child != null) {
+                child.kill()
+                child = null
+            }
+
+            child = spawn(process.argv[0], ["--enable-source-maps", "--inspect", "./build/index.mjs"], { stdio: "inherit" })
+        }
+
+        async function rebuildNow(params) {
+            if (debounceTimerId != null) {
+                clearTimeout(debounceTimerId)
+                debounceTimerId = null
+            }
+
+            if (esbuildContext == null) throw new TypeError("Build backend didn't return a context when isDev was true")
+            await esbuildContext.rebuild()
+        }
+
+        async function rebuild() {
+            if (debounceTimerId != null) {
+                clearTimeout(debounceTimerId)
+            }
+
+            debounceTimerId = setTimeout(() => {
+                debounceTimerId = null
+                rebuildNow().then(() => {
+                    if (date == lastDate) return
+                    log("Reloaded due to changes.")
+                    execute()
+                })
+            }, 1000)
+        }
+
+        process.stdin.on("data", (data) => {
+            if (data.toString() == "rs\n") {
+                log("Reloaded due to user command.")
+                execute()
+            }
+        })
+
+        const tsc = spawn("yarn tsc --noEmit --watch --incremental --preserveWatchOutput --pretty", {
+            stdio: "pipe",
+            shell: true,
+        })
+
+        let first = true
+        tsc.stdout.addListener("data", (chunk) => {
+            process.stdout.write(chunk)
+            if (chunk.toString().includes("Found 0 errors.")) {
+                if (first) {
+                    first = false
+                    rebuildNow().then(() => execute())
+                } else {
+                    rebuild()
+                }
+            }
+        })
+
+        tsc.stderr.addListener("data", (chunk) => {
+            process.stderr.write(chunk)
+        })
+    }
+
+    if (mode == "watch") {
+        await watchBackend()
+        return
+    }
+
+    if (mode == "dev") {
+        run("yarn vite", root)
+        await watchBackend()
+    }
+}, { desc: "Builds and or executes Node.js project :: Arguments: <build|watch|dev>", argc: 1 })
